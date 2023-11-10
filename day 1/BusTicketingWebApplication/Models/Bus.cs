578bb911a9f9a149bc2a26c655092d83d9e741f4
Vinay Penumaka <84056806+VinayChowdary244@users.noreply.github.com>
2023-11-10 10:07:47 +0530

    Add files via upload

---
+++ day 1/BusTicketingWebApplication/Models/Bus.cs
@@ -0,0 +1,15 @@
+﻿namespace BusTicketingWebApplication.Models
+{
+    public class Bus
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public float Cost { get; set; }
+        public int Capacity { get; set; }
+        public int BookedSeats { get; set; }
+        public int AvailableSeats { get; set; }
+
+
+
+    }
+}
