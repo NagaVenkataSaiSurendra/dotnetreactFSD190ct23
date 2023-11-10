e785a8976c1d764e8ad359e0d6797ee92637389c
Vinay Penumaka <84056806+VinayChowdary244@users.noreply.github.com>
2023-11-10 10:04:47 +0530

    Add files via upload

---
+++ day 1/BusTicketingWebApplication/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using BusTicketingWebApplication.Models.DTOs;
+using BusTicketingWebApplication.Models.DTOs;
+
+namespace BusTicketingWebApplication.Interfaces
+{
+    public interface ITokenService
+    {
+        string GetToken(UserDTO user);
+    }
+}
